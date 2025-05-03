6f4ddacd69222dafadb1a11e671ad23ec43587b7
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-05-03 12:14:32 +0600

    learn typescript

---
+++ module_1/dist/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const text = 'hello mehedi!';
+console.log(text);
+// Tuple
+const data = ['mehedi', 30, true];
---
+++ module_1/src/index.ts
@@ -0,0 +1,5 @@
+const text: string = 'hello mehedi!';
+console.log(text);
+
+// Tuple
+const data: [string, number, boolean] = ['mehedi', 30, true];
