f51454511d03ef76b281ae291f64b69367f94e8d
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-05-03 12:36:16 +0600

    learned object type

--- module_1/src/index.ts
+++ module_1/src/index.ts
@@ -1,5 +1,20 @@
+// String
 const text: string = 'hello mehedi!';
-console.log(text);
 
 // Tuple
 const data: [string, number, boolean] = ['mehedi', 30, true];
+
+// Reference type --> object
+const data2: {
+  company: 'Space X'; // literal type
+  fName: string;
+  lName?: string;
+  age: number;
+  isValid: boolean;
+} = {
+  company: 'Space X',
+  fName: 'Mehedi',
+  lName: 'Hasan',
+  age: 30,
+  isValid: true,
+};
