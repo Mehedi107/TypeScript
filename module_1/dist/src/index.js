b99fb42ddfe619cca7d084b0543f0c6a9c28c682
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-05-04 13:35:50 +0600

    learn object type

--- module_1/dist/src/index.js
+++ module_1/dist/src/index.js
@@ -1,5 +1,13 @@
 "use strict";
+// String
 const text = 'hello mehedi!';
-console.log(text);
 // Tuple
 const data = ['mehedi', 30, true];
+// Reference type --> object
+const data2 = {
+    company: 'Space X',
+    fName: 'Mehedi',
+    lName: 'Hasan',
+    age: 30,
+    isValid: true,
+};
