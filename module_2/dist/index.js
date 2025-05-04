3d9a2885fcd0de588f64d009ecb966cc10258f8b
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-05-04 18:30:43 +0600

    learn type assertion

---
+++ module_2/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// type assertion
+let text;
+text = 'string';
+text;
+text = 123;
+text;
+console.log('mehedi');
+// tsnd --respawn src/index.ts
---
+++ module_2/src/index.ts
@@ -0,0 +1,12 @@
+// type assertion
+let text: any;
+
+text = 'string';
+text as string;
+
+text = 123;
+text as number;
+
+console.log('mehedi');
+
+// tsnd --respawn src/index.ts
