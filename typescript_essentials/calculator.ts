e265ad8c3952559b6a37f42850004e01b6f84415
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-05-20 02:26:07 +0600

    create first custom type

--- typescript_essentials/calculator.ts
+++ typescript_essentials/calculator.ts
@@ -1,4 +1,17 @@
-function calculateInvestment(data) {
+// data:
+// initial amount
+// annual contribution
+// expected return
+// duration
+
+type InvestmentData = {
+  initialAmount: number;
+  annualContribution: number;
+  expectedReturn: number;
+  duration: number;
+};
+
+function calculateInvestment(data: InvestmentData) {
   // calculate with data...
 }
 
