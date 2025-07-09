b6e18164c85784e24144f174206e2fa2c8d964f5
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-07-09 18:34:29 +0600

    creating and using generic types

--- generics/generic.ts
+++ generics/generic.ts
@@ -1,1 +1,10 @@
 let names: Array<string> = ['max', 'john'];
+
+type DataStore<T> = {
+  [key: string]: T;
+};
+
+const store: DataStore<string | boolean> = {
+  fName: 'Max',
+  isAdult: false,
+};
