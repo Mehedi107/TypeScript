685ed368b9a7fe16b1d01de395f4d50c12a95ff6
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-11-12 21:09:35 +0600

    11. A First Glimpse At Generic Types - Alternative Array Type Declaration

--- Maximillian/02. TypeScript Basics & Basic Types/arrays-objects.ts
+++ Maximillian/02. TypeScript Basics & Basic Types/arrays-objects.ts
@@ -2,7 +2,8 @@ let hobbies = ['cooking', 'reading'];
 
 // hobbies.push(10);
 
-let users: (string | number)[];
+// let users: (string | number)[];
+let users: Array<string | number>;
 
 users = [5, 'Max'];
 users = [6, 1];
