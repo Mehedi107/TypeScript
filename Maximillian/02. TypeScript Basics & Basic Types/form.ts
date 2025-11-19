e56f37730e30eb03f6dc23a85e8dc25e1f551d2a
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-11-19 20:40:13 +0600

    26. Type Casting

--- Maximillian/02. TypeScript Basics & Basic Types/form.ts
+++ Maximillian/02. TypeScript Basics & Basic Types/form.ts
@@ -1,4 +1,4 @@
-const inputEl = document.querySelector('#user_name'); // "!" means the value will not be null.
+const inputEl = document.querySelector('#user_name') as HTMLInputElement | null; // "!" means the value will not be null.
 
 // if(!inputEl) {
 //   throw new Error('User name not found');
