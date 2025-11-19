013ebb73db9f214a84a75da799f48dd75e5f62aa
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-11-19 20:30:36 +0600

    25. Forced Not Null And Optional Chaining

--- Maximillian/02. TypeScript Basics & Basic Types/form.ts
+++ Maximillian/02. TypeScript Basics & Basic Types/form.ts
@@ -1,7 +1,7 @@
-const inputEl = document.querySelector('#user_name');
+const inputEl = document.querySelector('#user_name'); // "!" means the value will not be null.
 
-if(!inputEl) {
-  throw new Error('User name not found');
-}
+// if(!inputEl) {
+//   throw new Error('User name not found');
+// }
 
-console.log(inputEl.value);
+console.log(inputEl?.value);
