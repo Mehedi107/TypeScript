b7cb1b6ac134980c6f28c4534decd9e99294f6de
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-12-14 22:08:26 +0600

    1-5 Object , Optional and Literal Types

--- Programming-Hero/Module-1/src/index.ts
+++ Programming-Hero/Module-1/src/index.ts
@@ -13,3 +13,20 @@ let e : string[] = ["a", 'b', 'c'];
 let f : number[] = [2,3,4];
 let g : [string, number] = ['Mehedi', 20]; 
 let h : boolean = false;
+
+
+// Reference types (object)
+///////////////////////////
+
+const user : {
+  firstName: string,
+  lastName: string,
+  isMarried: boolean,
+  readonly company: string,
+  // company : 'Mehedi Mart', // literal type
+} = {
+  firstName: 'Mehedi',
+  lastName: 'Hasan',
+  isMarried: false,
+  company: 'Mehedi Mart'
+}
