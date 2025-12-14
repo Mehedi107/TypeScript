bf2a50fa63192e6f4b92d2b1421ae7be831e4c6d
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-12-14 21:58:06 +0600

    1-4 Basic data types

--- Programming-Hero/Module-1/src/index.ts
+++ Programming-Hero/Module-1/src/index.ts
@@ -1,3 +1,15 @@
 const course: string = "Next level web development";
 
 console.log(course);
+
+// Basic Data Types
+///////////////////
+
+let a : string = 'Mehedi';
+let b : number = 10;
+let c : undefined = undefined;
+let d : null = null;
+let e : string[] = ["a", 'b', 'c'];
+let f : number[] = [2,3,4];
+let g : [string, number] = ['Mehedi', 20]; 
+let h : boolean = false;
