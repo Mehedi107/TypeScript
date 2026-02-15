04dbe8cbe5ec9940f4bf6e5b97e04476d4f4bec8
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2026-02-15 21:02:12 +0600

    3-2: Inheritance in OOP

--- Programming-Hero/Module-3/src/index.ts
+++ Programming-Hero/Module-3/src/index.ts
@@ -1,3 +1,5 @@
+// ✅ 3-1: Class and object
+
 class Animal {
   constructor(public name: string, public species: string, public sound: string) {}
 
@@ -10,3 +12,40 @@ const dog = new Animal('Ben', 'dog', 'Bark')
 const cat = new Animal('Puchi', 'cat', 'mew')
 
 cat.makeSound()
+
+// ✅ 3-2: Inheritance in OOP
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getInfo() {
+    console.log(`${this.name} is ${this.age} years old.`);
+  }
+}
+
+class Student extends Person {
+  studentId: string;
+
+  constructor(name: string, age: number, studentId: string) {
+    super(name, age);
+    this.studentId = studentId;
+  }
+}
+
+class Teacher extends Person {
+  designation: string;
+
+  constructor(name: string, age: number, designation: string) {
+    super(name, age);
+    this.designation = designation;
+  }
+}
+
+const student1 = new Student('Mehedi', 10, '11586')
+const teacher1 = new Teacher('Max', 20, 'Teacher')
