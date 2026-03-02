ca4f608f918055ac46e8482df15be17a3bc41ca1
Mehedi <mehedi.hsn.dev@gmail.com>
2026-03-02 15:59:47 +0600

    union and any type

--- Chai-Aur-Typescript/class-1/src/index.ts
+++ Chai-Aur-Typescript/class-1/src/index.ts
@@ -9,3 +9,7 @@ let str: string = 'Mehedi'
 let bol: boolean = false
 
 let value = Math.random() > 0.5 ? 10 : "5";
+
+// ✅ union and any type
+let value2: string | number = '5'
+const apiRequestStatus: 'pending' | 'success' | 'error' = "success"
