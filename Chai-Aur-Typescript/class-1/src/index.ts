2f4b4c92209547875d6f29995ed57e835324774e
Mehedi <mehedi.hsn.dev@gmail.com>
2026-03-02 16:00:15 +0600

    union and any type

--- Chai-Aur-Typescript/class-1/src/index.ts
+++ Chai-Aur-Typescript/class-1/src/index.ts
@@ -13,3 +13,14 @@ let value = Math.random() > 0.5 ? 10 : "5";
 // ✅ union and any type
 let value2: string | number = '5'
 const apiRequestStatus: 'pending' | 'success' | 'error' = "success"
+
+let arr = [1,4,6,7]
+let currentValue;
+
+for(let a of arr) {
+  if(a === 4) {
+    currentValue = a
+    break
+  }
+}
+console.log(currentValue);
