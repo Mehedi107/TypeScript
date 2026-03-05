ac30d29df6d6baf073b66b58e4461b95c9746725
Mehedi <mehedi.hsn.dev@gmail.com>
2026-03-05 15:05:15 +0600

    type narrowing

--- Chai-Aur-Typescript/class-1/src/index.ts
+++ Chai-Aur-Typescript/class-1/src/index.ts
@@ -24,3 +24,49 @@ for(let a of arr) {
   }
 }
 console.log(currentValue);
+
+// ✅ type narrowing
+
+type value = string | number
+
+function getValue(input: value) {
+  if(typeof input === 'string') {
+    console.log(`The value is string type.`);
+  }
+  console.log(`The value is number type.`);
+}
+
+function serveChai(msg?: string): string {
+  if(msg) {
+    return `Serving ${msg} chai`
+  }
+  return `Serving chai without message.`
+}
+
+function orderChai(size: 'small'|'medium'|'large'):string {
+  if(size === 'small') return `Small tea...`
+  if(size === 'medium') return `medium tea...`
+  if(size === 'large') return `large tea...`
+
+  return `No size selected for tea`
+}
+
+class MilkTea {
+  server() {
+    return `Serving milk tea.`
+  }
+}
+
+class BlackTea {
+  server() {
+    return `Serving black tea.`
+  }
+}
+
+function serveTea(teaType: MilkTea | BlackTea) {
+  if(teaType instanceof MilkTea) {
+    return `${teaType.server()}`
+  }
+  
+  return `${teaType.server()}`
+}
